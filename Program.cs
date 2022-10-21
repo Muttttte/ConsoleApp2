016f87d5da9df67ecd8369f2994eb20859d28815
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-21 14:54:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Uppgift_4._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i2 = 200; i2 >= 180; i2--)
+            {
+                Console.WriteLine(i2);
+            }
+            for (int Tall = 1000; Tall <= 1400; Tall += 50)
+            {
+                Console.WriteLine(Tall);
+            }
+
+        }
+
+    }
+}
